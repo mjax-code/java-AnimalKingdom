9bf3f0efc3ac0f7140bd3ea87af4df4083a64070
mjax-code <mjax.code@gmail.com>
2019-02-06 22:51:44 -0800

    finished logic

--- src/com/mjax/Main.java
+++ src/com/mjax/Main.java
@@ -3,6 +3,7 @@ package com.mjax;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class Main {
 
@@ -16,6 +17,57 @@ public class Main {
        for (AnimalsAbstract animal : animals) {
            System.out.println(animal.getName());
        }
+    }
+
+    public static void alphabetical(ArrayList<AnimalsAbstract> animals){
+        Collections.sort(animals, (o1,o2) -> {
+            return o1.getName().compareTo(o2.getName());
+        });
+
+        for (AnimalsAbstract animal : animals) {
+            System.out.println(animal.getName());
+        }
+    }
+
+    public static void movement(ArrayList<AnimalsAbstract> animals) {
+        Collections.sort(animals, Comparator.comparing((AnimalsAbstract animal) -> animal.move()));
+        for (AnimalsAbstract animal : animals) {
+            System.out.println(animal.getName());
+        }
+    }
+
+    public static void lungs(ArrayList<AnimalsAbstract> animals) {
+        ArrayList<AnimalsAbstract> newList = new ArrayList<>(animals);
+
+        newList.removeIf((AnimalsAbstract animal) -> (!animal.breath().equals("lungs")));
+        for (AnimalsAbstract animal : newList) {
+            System.out.println(animal.getName());
+        }
+    }
+
+    public static void lungs1758(ArrayList<AnimalsAbstract> animals) {
+        ArrayList<AnimalsAbstract> newList = new ArrayList<>(animals);
+
+        newList.removeIf((AnimalsAbstract animal) -> (!animal.breath().equals("lungs") || animal.getYear() != 1758));
+        System.out.println("lungs and 1758");
+        for (AnimalsAbstract animal : newList) {
+            System.out.println(animal.getName());
+        }
+    }
+    public static void lungsEggs(ArrayList<AnimalsAbstract> animals) {
+        ArrayList<AnimalsAbstract> newList = new ArrayList<>(animals);
+
+        newList.removeIf((AnimalsAbstract animal) -> (!animal.breath().equals("lungs") || !animal.reproduce().equals("eggs")));
+        System.out.println("lungs and eggs");
+        for (AnimalsAbstract animal : newList) {
+            System.out.println(animal.getName());
+        }
+    }
+
+    public static ArrayList<AnimalsAbstract> only1758(ArrayList<AnimalsAbstract> animals){
+        ArrayList<AnimalsAbstract> newList = new ArrayList<>(animals);
+        newList.removeIf((AnimalsAbstract animal) -> (animal.getYear() != 1758));
+        return newList;
     }
 
     public static void main(String[] args) {
@@ -46,5 +98,19 @@ public class Main {
         }
         System.out.println("\n");
         orderAnimalsByYear(animalsList);
+        System.out.println("\n");
+        alphabetical(animalsList);
+        System.out.println("\n");
+        movement(animalsList);
+        System.out.println("\n");
+        lungs(animalsList);
+        System.out.println("\n");
+        lungs1758(animalsList);
+        System.out.println("\n");
+        lungsEggs(animalsList);
+        System.out.println("\n");
+        alphabetical(only1758(animalsList));
+
+
     }
 }
